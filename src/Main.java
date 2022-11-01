6d1ee68e04fe15d057225cf7d5060d7f5e2340a8
Max Zavalo <max.mine2104@gmail.com>
2022-11-01 21:48:53 +0300

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Matvei lox");
+        short a = 123;
+        long b = 123213213;
+        System.out.printf(a + " + " + b);
     }
 }
